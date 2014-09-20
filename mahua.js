66a342f39f125476c9b41e96d31bcbb7477cb11d
胡伯 <hubo.hb@taobao.com>
2014-09-20 13:26:25 +0800

    init

---
+++ mahua.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+var http = require("http")
+  , path = require("path")
+  , mime = require("mime")
+  , url = require("url")
+  , fs = require("fs")
+  , port = process.env.PORT || 8889;
+
+// compatibility with node 0.6
+if (!fs.exists)
+  fs.exists = path.exists;
+
+http.createServer(function(request, response) {
+
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
+  
+  fs.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      var contentType = mime.lookup(filename) || "text/plain";
+      response.writeHead(200, {"Content-Type": contentType});
+      console.log(file)
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(port, "0.0.0.0");
+
+console.log("http://localhost:" + port);
